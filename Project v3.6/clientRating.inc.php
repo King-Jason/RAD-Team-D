5e74ffbe2bbb48f1fa01d9785e58ef524251bfd4
Kyle C <54973192+kyle623@users.noreply.github.com>
2019-12-04 23:37:04 +0800

    Improved UI Experience on main page and features

---
+++ Project v3.6/clientRating.inc.php
@@ -0,0 +1,50 @@
+<!--
+ Version 1: Original Document: php for checking password and starting sessions Jason King
+ Version 1.1: Made website responsive : Jason King
+ Version 1.2: Minor php code convention fixes using PHP_CodeSniffer and PHP Beautifier
+ 
+ Copyright to Acme Entertainment Pty Ltd
+-->
+<?php
+session_start();
+$userEmail = $_SESSION['user_id'];
+$mID = $_POST['MID'];
+$sRating = $_POST['SRating'];
+$userID = "";
+
+$conn = mysqli_connect('127.0.0.1:3306', 'root', '', 'smtmoviesrental');
+
+if (!$conn) {
+    die("Connection to servers failed. Error: " . mysqli_connect_error());
+}
+
+
+$sql = "SELECT * FROM users WHERE Email=?";
+$stmt = mysqli_stmt_init($conn);
+if(!mysqli_stmt_prepare($stmt, $sql)) {
+    header("Location: clientPage.php?rating=sqlerror");
+    exit();
+}else{
+    mysqli_stmt_bind_param($stmt, "s", $userEmail);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    if($row = mysqli_fetch_assoc($result)) {
+        $userID = $row['ID'];
+    }else{
+        header("Location: clientPage.php?rating=nouser");
+    }
+}
+
+
+$sql = "INSERT INTO moviesrating (movieID, Rating, userID) VALUES (?,?,?)";
+if(!mysqli_stmt_prepare($stmt, $sql)) {
+    header("Location: clientPage.php?rating=sqlerror");
+}else {
+    mysqli_stmt_bind_param($stmt, "sss", $mID, $sRating, $userID);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_store_result($stmt);    
+    header("Location: clientPage.php?rating=Rated");
+    exit();
+}
+
+?>
---
+++ Project v3.6/resetPassword.inc.php
@@ -0,0 +1,61 @@
+<!--
+ Version 1: Original Document: php for admin login Jason King
+ Version 1.1: Made website responsive : Jason King
+ Version 1.2: Minor php code convention fixes using PHP_CodeSniffer and PHP Beautifier : Kyle Cleofe
+ 
+ Copyright to Acme Entertainment Pty Ltd
+-->
+<?php
+$email = $_POST['Email'];
+$cPassword = $_POST['CPassword'];
+$nPassword = $_POST['NPassword'];
+
+$conn = mysqli_connect('127.0.0.1:3306', 'root', '', 'smtmoviesrental');
+
+if (!$conn) {
+    die("Connection to servers failed. Error: " . mysqli_connect_error());
+}
+
+$uppercase = preg_match('@[A-Z]@', $nPassword);
+$lowercase = preg_match('@[a-z]@', $nPassword);
+$number    = preg_match('@[0-9]@', $nPassword);
+$specialChars = preg_match('@[^\w]@', $nPassword);
+if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($nPassword) < 8) {
+    header("Location: AdminPanel.php?error=passwordstrength");
+}else {
+    $sql = "SELECT * FROM `users` WHERE `Email`=?";
+    $stmt = mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt, $sql)) {
+        header("Location: AdminPanel.php?error=sqlerror");
+        exit();
+    }else{
+        mysqli_stmt_bind_param($stmt, "s", $email);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        if($row = mysqli_fetch_assoc($result)) {
+            $pwdCheck = password_verify($cPassword, $row['Password']);
+            if($pwdCheck == false) {
+                header("Location: AdminPanel.php?error=error");
+                exit();
+            }
+            else if($pwdCheck == true) {
+                $sql = "UPDATE users SET Password=? WHERE Email=?";
+                $stmt = mysqli_stmt_init($conn);
+                if(!mysqli_stmt_prepare($stmt, $sql)) {
+                    echo "There was an error!";
+                }else {
+                    $newPwdHash = password_hash($nPassword, PASSWORD_DEFAULT);
+                    mysqli_stmt_bind_param($stmt, "ss", $newPwdHash, $email);
+                    mysqli_stmt_execute($stmt);
+                    header("Location: AdminPanel.php?reset=success");
+                }
+            }
+        }else {
+            header("Location: AdminPanel.php?error=nouser");
+        }
+    }
+}
+    
+
+
+?>
